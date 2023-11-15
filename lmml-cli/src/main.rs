c7220d1e2f7935159f1e2e2e9da44e456b94b789
yuma14 <yuma140902@gmail.com>
2023-11-16 01:33:35 +0900

    エラー時のメッセージ表示

--- lmml-cli/src/main.rs
+++ lmml-cli/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -5,7 +6,7 @@ pub struct Args {
     lmml: String,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     println!("lmml:");
@@ -23,14 +24,38 @@ fn main() {
     println!("{:#?}", ast);
     println!();
 
-    if let Ok((_, ast)) = ast {
-        let timeline = ast.to_timeline();
-        println!("=== Timeline ===");
-        println!("{}", timeline);
+    let ast = match ast {
+        Err(err) => {
+            match err {
+                nom::Err::Incomplete(_) => {
+                    eprintln!("nom::Err::Incomplete");
+                }
+                nom::Err::Error(e) | nom::Err::Failure(e) => {
+                    eprintln!("{}", input);
+                    let pos = input.chars().count() - e.input.chars().count();
+                    for _ in 0..pos {
+                        eprint!(" ");
+                    }
+                    eprintln!("^");
+                }
+            }
+            anyhow::bail!("LMMLに構文エラーがあります")
+        }
+        Ok((_, ast)) => ast,
+    };
 
-        let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-        let sink = rodio::Sink::try_new(&stream_handle).unwrap();
-        timeline.play(&sink);
-        sink.sleep_until_end();
-    }
+    let timeline = ast.to_timeline();
+    println!("=== Timeline ===");
+    println!("{}", timeline);
+
+    let (_stream, stream_handle) = rodio::OutputStream::try_default().with_context(|| {
+        "音声出力ストリームの取得に失敗しました。Windows WASAPIでのみ動作確認しています。"
+    })?;
+    let sink = rodio::Sink::try_new(&stream_handle).with_context(|| {
+        "音声出力の作成に失敗しました。Windows WASAPIでのみ動作確認しています。"
+    })?;
+    timeline.play(&sink);
+    sink.sleep_until_end();
+
+    Ok(())
 }
